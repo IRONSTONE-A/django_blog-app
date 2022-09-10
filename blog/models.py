1f902af49ee4450d8d440dda5ef79f74b3fc50f0
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-09-10 22:47:39 +0300

    model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
 
 # Create your models here.
+class Post(models.Model):
+    OPTIONS= (
+        ("draft", "Draft")
+        ("p", "Published")
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default="d")
+    slug = models.SlugField(blank=True, unique=True)
