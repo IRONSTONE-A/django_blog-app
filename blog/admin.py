a1a304cb4c752b20fa30cdbf47b27826d57f5210
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-09-10 23:17:38 +0300

    admin pannel created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
+from atexit import register
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,23 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
+    
+    
+    class Meta:
+        verbose_name_plural= "Categories"
+        
+    def __str__(self):
+        return self.name
 
 # Create your models here.
 class Post(models.Model):
     OPTIONS= (
-        ("draft", "Draft")
-        ("p", "Published")
+        ("draft", "Draft"),
+        ("p", "Published"),
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -21,3 +28,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default="d")
     slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
