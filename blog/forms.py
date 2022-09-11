10c6e61a6853450ac53d26d6cd125eba639831ec
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-09-11 08:48:26 +0300

    forms created

--- blog/forms.py
+++ blog/forms.py
@@ -1,2 +1,21 @@
 from socket import fromshare
 from django import forms
+from .models import Category, Post, Comment
+
+class PostForm(forms.ModelForm):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select")
+    class Meta:
+        model = Post
+        fielsds= (
+            "title",
+            "content",
+            "image",
+            "category",
+            "status",
+        )
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("content",)
